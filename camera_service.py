c78715a5cefcb83c154d8b27634557949f028a91
Tino Lang <tino.lang@desy.de>
2025-10-20 14:24:25 +0200

    inceased max exposure time to 100000

--- camera_service.py
+++ camera_service.py
@@ -434,7 +434,7 @@ class CameraService:
             except (VmbFeatureError, AttributeError, TypeError, ValueError):
                 inc = None
 
-            ui_min, ui_max, ui_step = 1000, 50000, 1000
+            ui_min, ui_max, ui_step = 1000, 100000, 1000
             requested = float(requested_us)
 
             target = max(min_v, min(max_v, requested))
